a19a74b93b6de2ee6d4b66c46721ed87942e8468
kevil <kevin.inck@gmail.com>
2024-04-27 13:41:26 -0400

    class gitpack

---
+++ src/main/java/com/jitpackexample/demo/HelloJitpack.java
@@ -0,0 +1,7 @@
+package com.jitpackexample.demo;
+
+public class HelloJitpack {
+    public static void sayHello() {
+        System.out.println("Hola desde Jitpack. De mi repositorio Kevil17");
+    }
+}
